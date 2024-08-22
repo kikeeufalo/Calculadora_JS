a7c19a3918a2a67804ab0a6c39e832a53c85f974
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-08-22 20:24:15 -0300

    Criando a logica

---
+++ JS/funcCalc.js
@@ -0,0 +1,73 @@
+var tela = window.document.getElementById("textRess")
+var a = 0
+var b = 0
+tela.innerText = 0
+
+function mais() {
+    var soma = a + b
+    tela.innerText += "+"
+}
+
+function rese() {
+    tela.innerText = 0
+}
+function num1() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 1
+}
+function num2() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 2
+}
+function num3() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 3
+}
+function num4() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 4
+}
+function num5() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 5
+}
+function num6() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 6
+}
+function num7() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 7
+}
+function num8() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 8
+}
+function num9() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 9
+}
+function num0() {
+    if (tela.innerText == 0) {
+        tela.innerText = null
+    }
+    tela.innerText += 0
+}
