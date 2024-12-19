c978802388e7998c9fe44c1fe49521cdcdf3d157
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-12-19 20:11:31 -0300

    Update funcCalc.js

--- JS/funcCalc.js
+++ JS/funcCalc.js
@@ -1,8 +1,19 @@
-let containerNumeros = document.querySelector('.botoesNumeros')
-let numeros = containerNumeros.querySelectorAll('button')
+const containerNumeros = document.querySelector('.botoesNumeros')
+const containerOperadores = document.querySelector('.botoes-operadores')
+const operadores = containerOperadores.querySelectorAll('button')
+const numeros = containerNumeros.querySelectorAll('button')
 
+let buttonClicadosA = ''
+
+
+
+
+// parte dos numeros/valores dos botoes
 numeros.forEach((num) => {
     num.addEventListener('click', function() {
-        console.log(num.textContent)
+        
+        botoesClicados += num.textContent
+
+        console.log(botoesClicados)
     })
 })
